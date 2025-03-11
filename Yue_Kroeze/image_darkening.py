75ea38382a3171ec631911fcade625fb2997a0f4
YueKroeze <63613466+Mika-08@users.noreply.github.com>
2025-03-11 13:50:52 +0100

    Initial testing with the hourglass models

---
+++ Yue_Kroeze/image_darkening.py
@@ -0,0 +1,18 @@
+from PIL import Image, ImageEnhance
+
+def reduce_brightness(image_path, output_path, factor):
+    # Open an image file
+    with Image.open(image_path) as img:
+        # Enhance the image's brightness
+        enhancer = ImageEnhance.Brightness(img)
+        img_enhanced = enhancer.enhance(factor)
+        # Save the result
+        img_enhanced.save(output_path)
+        print(f"Image saved as {output_path}")
+
+# Example usage:
+image_path = 'Images/099548361.jpg'
+output_path = 'Darker_Images/099548361.jpg.jpg'
+brightness_factor = 0.5
+
+reduce_brightness(image_path, output_path, brightness_factor)
